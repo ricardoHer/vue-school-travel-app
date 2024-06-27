7357ca006f871dec705b4da94880bd057837ccf0
Ricardo FRanco Hernandez <ricardo.hern@icloud.com>
2024-06-27 14:45:11 -0300

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <TheNavigation />
+  <div class="container">
+    <router-view :key="$route.path" />
+  </div>
+</template>
+
+<script>
+import TheNavigation from "@/components/TheNavigation.vue";
+export default {
+  components: {
+    TheNavigation,
+  },
+};
+</script>
---
+++ src/router/index.js
@@ -0,0 +1,31 @@
+import { createRouter, createWebHistory } from "vue-router";
+import HomeView from "../views/HomeView.vue";
+
+const routes = [
+  {
+    path: "/",
+    name: "home",
+    component: HomeView,
+  },
+  {
+    path: "/destination/:id/:slug",
+    name: "destination.show",
+    component: () => import("@/views/DestinationShow.vue"),
+    props: (route) => ({ ...route.params, id: parseInt(route.params.id) }),
+    children: [
+      {
+        path: ":experienceSlug",
+        name: "experience.show",
+        component: () => import("@/views/ExperienceShow.vue"),
+        props: (route) => ({ ...route.params, id: parseInt(route.params.id) }),
+      },
+    ],
+  },
+];
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes,
+});
+
+export default router;
